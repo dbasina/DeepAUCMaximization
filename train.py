23c2d72bb55b5b0c761780295b1bc1dbf0497ff4
dbasina <dbasina@asu.edu>
2025-10-28 08:59:34 -0700

    swin+deepAUC

--- train.py
+++ train.py
@@ -1,33 +1,37 @@
 import torch
 import libauc
+import timm
 from libauc.losses import MultiLabelAUCMLoss, AUCMLoss
 from libauc.optimizers import PESG
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from train_utils import train_one_epoch, lr_lambda
+import torchvision.transforms as transforms
+from torchvision.transforms import InterpolationMode
+from train_utils import train_one_epoch
 import os
-from dataloader import CheXpert
+from dataset import CheXpert
 from models import create_ensemble
 from tqdm import trange,tqdm
 from torch.utils.data import Subset
 from torch.optim.lr_scheduler import LambdaLR
 
 # Define paths, create necessary directories
-# train_csv_path = "./CheXpert-mini/train.csv"
-# val_csv_path = "./CheXpert-mini/valid.csv"
-# test_csv_path = "./CheXpert-mini/test.csv"
-# train_images_path = "./CheXpert-mini/train"
-# val_images_path = "./CheXpert-mini/valid"
-# test_images_path = "./CheXpert-mini/test"
-# image_root_path = "./CheXpert-mini"
 
-train_csv_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/train.csv"
-val_csv_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/valid.csv"
-test_csv_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/test.csv"
-train_images_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/train"
-val_images_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/valid"
-test_images_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/test"
-image_root_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0"
+train_csv_path = "./CheXpert-mini/train.csv"
+val_csv_path = "./CheXpert-mini/valid.csv"
+test_csv_path = "./CheXpert-mini/test.csv"
+train_images_path = "./CheXpert-mini/train"
+val_images_path = "./CheXpert-mini/valid"
+test_images_path = "./CheXpert-mini/test"
+image_root_path = "./CheXpert-mini/"
+
+# train_csv_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/train.csv"
+# val_csv_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/valid.csv"
+# test_csv_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/test.csv"
+# train_images_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/train"
+# val_images_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/valid"
+# test_images_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0/test"
+# image_root_path = "/scratch/dbasina/CheXpert-v1.0/CheXpert-v1.0"
 os.makedirs("Outputs", exist_ok=True)
 os.makedirs("Outputs/models", exist_ok=True)
 os.makedirs("Outputs/logs", exist_ok=True)
@@ -38,13 +42,11 @@ batch_size = 4
 num_epochs = 2
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = create_ensemble().to(device)
-model = nn.DataParallel(model)
 
 # Checkpoint loading (if any)
 # checkpoint = torch.load('./Outputs/models/run2_best_model.pth', map_location=device)
 # model.load_state_dict(checkpoint)
-print("Checkpoint Loaded")
+
 
 train_ds = CheXpert(
     csv_path=train_csv_path,
@@ -53,6 +55,7 @@ train_ds = CheXpert(
     mode="train",
     use_frontal=True,
     use_upsampling=True,
+    class_index=-1,
 )
 val_ds = CheXpert(
     csv_path=val_csv_path,
@@ -61,14 +64,16 @@ val_ds = CheXpert(
     mode="valid",
     use_frontal=True,      # set False if valid.csv lacks the column
     use_upsampling=False,
+    class_index=-1,
 )
 test_ds = CheXpert(
     csv_path=test_csv_path,
     image_root_path=image_root_path,
     image_size=320,
     mode="valid",
-    use_frontal=True,      # set False if test.csv lacks the column
+    use_frontal=False,      # set False if test.csv lacks the column
     use_upsampling=False,
+    class_index=-1,
 )
 
 subset_train_indices = list(range(100))
@@ -85,33 +90,43 @@ test_loader  = DataLoader(test_ds,  batch_size=batch_size, shuffle=False, num_wo
 
 # Define running AUC
 bestAUC = 0.0
-milestones = {2000, 8000}
 
-#Train Stage 1: Using BCEWithLogitsLoss and Adam optimizer for warm-up
-stage = 1
-# loss = nn.BCEWithLogitsLoss()
-loss = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
-log_file.write(f"Starting Stage {stage} Training\n")
-log_file.flush()
-for epoch in trange(num_epochs, desc = "Epochs"):
-    train_loss, bestAUC = train_one_epoch(train_subset_loader, val_subset_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device, milestones)
-    # train_loss, bestAUC = train_one_epoch(train_loader,test_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device, milestones)
+
+model = timm.create_model('swin_base_patch4_window7_224', pretrained=True, num_classes = 5, in_chans = 3)
+model = model.to(device)
+model = nn.DataParallel(model)
+
+# # Train Stage 1: Using BCEWithLogitsLoss and Adam optimizer for initial training
+# stage = 1
+# imratio_list = getattr(train_ds, "imratio_list", None)
+# # loss = nn.BCEWithLogitsLoss()
+# loss = nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+# log_file.write(f"Starting Stage {stage} Training\n")
+# log_file.flush()
+# for epoch in trange(num_epochs, desc = "Epochs"):
+#     train_loss, bestAUC = train_one_epoch(train_subset_loader, val_subset_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device, milestones)
+#     # train_loss, bestAUC = train_one_epoch(train_loader,test_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device, milestones)
 
 
 #Train Stage 2: Using MultiLabelAUCMLoss and PESG optimizer for finetuning, reset the final fc layer
 stage = 2
-for gamma in [1/300, 1/500, 1/800]:
-    model.module.fc.reset_parameters()
-    imratio_list = getattr(train_ds, "imratio_list", None)
-    loss = MultiLabelAUCMLoss(num_labels=5, device = device, imratio = imratio_list, margin = 0.8)
-    optimizer = PESG(model.parameters(),loss_fn = loss, lr=0.1, weight_decay=0, epoch_decay = gamma, mode = 'sgd', device=device,verbose=True)
-    log_file.write(f"Starting Stage {stage} Training\n")
-    log_file.flush()
-    for epoch in trange(num_epochs, desc="Epochs"):
-        train_loss, bestAUC = train_one_epoch(train_subset_loader,val_subset_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device, milestones)
-        # train_loss, bestAUC = train_one_epoch(train_loader,test_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device, milestones)
-        optimizer.update_regularizer()
+imratio_list = getattr(train_ds, "imratio_list", None)
+learning_rate = 0.1
+weight_decay = 1e-5
+epoch_decay = 2e-3
+margin = 1.0
+
+# loss = AUCMLoss(num_labels=5, device = device)
+
+loss = MultiLabelAUCMLoss(num_labels=5, device = device, imratio = imratio_list)
+optimizer = PESG(model.parameters(),loss_fn = loss, lr=learning_rate, margin=margin, weight_decay=weight_decay, epoch_decay = epoch_decay, device=device)
+log_file.write(f"Starting Stage {stage} Training\n")
+log_file.flush()
+for epoch in trange(num_epochs, desc="Epochs"):
+    # train_loss, bestAUC = train_one_epoch(train_subset_loader,val_subset_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device)
+    train_loss, bestAUC = train_one_epoch(train_loader,val_loader,test_loader, model, optimizer, loss, bestAUC, stage, epoch, log_file, device)
+    optimizer.update_regularizer(decay_factor=10)
         
 
 # Save the final model
--- train_utils.py
+++ train_utils.py
@@ -3,35 +3,11 @@ import torch.nn as nn
 from torch.nn.utils import clip_grad_norm_
 from models import create_ensemble
 import numpy as np
-from sklearn.metrics import roc_auc_score
+from libauc.metrics import auc_roc_score
 from tqdm import tqdm
 
-def lr_lambda(current_iter):
-    if current_iter < 2000:
-        return 1.0
-    elif current_iter < 8000:
-        return 1/3
-    else:
-        return 1/9
 
-def compute_AUC(val_labels, val_predictions):
-    try:
-        per_class = roc_auc_score(val_labels, val_predictions, average=None)
-        val_AUC_mean = float(np.nanmean(per_class))  # ignore classes that are NaN
-    except ValueError:
-        # happens if a class has only one label present; treat as NaN then mean later
-        per_class = []
-        for c in range(val_labels.shape[1]):
-            y = val_labels[:, c]
-            p = val_predictions[:, c]
-            try:
-                per_class.append(roc_auc_score(y, p))
-            except ValueError:
-                per_class.append(np.nan)
-        val_AUC_mean = float(np.nanmean(per_class))
-    return val_AUC_mean, per_class
-
-def train_one_epoch(dataloader, val_loader, model, optimizer, loss_fn, bestAUC, stage, epoch, log_file, device, milestones):
+def train_one_epoch(dataloader, val_loader,test_loader, model, optimizer, loss_fn, bestAUC, stage, epoch, log_file, device):
     model.train()
     total_loss = 0.0
     progress_bar = tqdm(dataloader, desc="Training", leave=False)
@@ -41,43 +17,27 @@ def train_one_epoch(dataloader, val_loader, model, optimizer, loss_fn, bestAUC, 
 
         optimizer.zero_grad()
         outputs = model(images)
+        outputs = torch.sigmoid(outputs)
         loss = loss_fn(outputs, labels)
+
+        optimizer.zero_grad()
         loss.backward()
-        clip_grad_norm_(model.parameters(), max_norm=2.0)
         optimizer.step()
-        if stage == 2:
-            gstep = optimizer.optim_step
-
-        if stage == 2 and gstep in milestones:
-            optimizer.update_lr(decay_factor=3)
         total_loss += loss.item() * images.size(0)
-
         progress_bar.set_postfix({"step": step + 1})
 
         #validation segment
         if (step + 1) % 50 == 0:
-            model.eval()
-            val_predictions = []
-            val_labels = []
-            with torch.no_grad():
 
-                for images, labels in val_loader:
-                    images = images.to(device)
-                    labels = labels.to(device)
-                    val_outputs = model(images)
-                    val_predictions.append(torch.sigmoid(val_outputs).cpu())
-                    val_labels.append(labels.cpu())
-            val_predictions = torch.cat(val_predictions).numpy()
-            val_labels = torch.cat(val_labels).numpy()
-            val_AUC_mean, per_class = compute_AUC(val_labels, val_predictions)
-            
+            val_mAUC = evaluate_AUC(model, val_loader, device)
+            test_mAUC = evaluate_AUC(model, test_loader, device)
             # Save the best model
-            if val_AUC_mean > bestAUC:
-                bestAUC = val_AUC_mean
-                torch.save(model.state_dict(), f"Outputs/models/best_model_gamma{gamma:.6f}.pth")
-            
+            if val_mAUC > bestAUC:
+                bestAUC = val_mAUC
+                torch.save(model.state_dict(), f"Outputs/models/best_model.pth")
+                
             # Log Results
-            log_file.write(f"Stage {stage}, Epoch {epoch}, Step {step+1}, Gamma {optimizer.epoch_decay}, Validation AUC: {val_AUC_mean:.4f}, Current Best AUC: {bestAUC:.4f}, Per-Class AUC: {per_class}\n")
+            log_file.write(f"Stage {stage}, Epoch {epoch}, Step {step+1}, Val AUC: {val_mAUC:.4f}, Test AUC: {test_mAUC:.4f}, Best Val AUC: {bestAUC:.4f}\n")
             log_file.flush()
             model.train()
         
@@ -86,3 +46,19 @@ def train_one_epoch(dataloader, val_loader, model, optimizer, loss_fn, bestAUC, 
 
     avg_loss = total_loss / len(dataloader.dataset)
     return avg_loss, bestAUC
+
+def evaluate_AUC(model, loader, device):
+    model.eval()
+    eval_predictions = []
+    eval_labels = []
+    with torch.no_grad():
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            eval_predictions.append(torch.sigmoid(outputs).cpu())
+            eval_labels.append(labels.cpu())
+    eval_predictions = torch.cat(eval_predictions).numpy()
+    eval_labels = torch.cat(eval_labels).numpy()
+    AUC_mean = np.mean(auc_roc_score(eval_labels, eval_predictions))
+    return AUC_mean
